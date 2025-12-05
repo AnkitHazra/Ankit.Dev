6e24c9fedfafa352e04e7f84992e229fedc48cc3
Ankit <hazraankit668@gmail.com>
2025-12-05 22:45:35 +0530

    Add SEO meta tags and sitemap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,8 @@ function Navbar({ menuOpen, setMenuOpen }) {
                 <div className='flex justify-between items-center h-16'>
                     <a href="https://github.com/AnkitHazra" className='front-mono text-3xl font-bold text-white'>
                         {" "}
-                        Ankit
-                        <span className='text-blue-500'>.dev</span>
+                        AnkitHazra
+                        <span className='text-blue-500'>.online</span>
                     </a>
                     <div className='w-7 h-5 relative cursor-pointer z-40 md:hidden' onClick={() => setMenuOpen((prev) => !prev)}>
                         &#9776;
